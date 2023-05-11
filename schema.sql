659bc0b4954eb155d7f95b7607ec29fdf8eea9d6
billygds <vasilisgoudis1@gmail.com>
2023-05-11 20:49:39 +0300

    new tables

--- schema.sql
+++ schema.sql
@@ -22,6 +22,7 @@ CREATE TABLE IF NOT EXISTS school (
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY  (school_id),
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS book (
   book_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   title VARCHAR(45) NOT NULL,
@@ -31,5 +32,68 @@ CREATE TABLE IF NOT EXISTS book (
   num_of_pages INT UNSIGNED NOT NULL,
   summary VARCHAR(1024) NOT NULL,
   available_copies INT UNSIGNED NOT NULL,
-)
+  images
+  category VARCHAR(15) NOT NULL,
+  language_of_book VARCHAR(15) NOT NULL,
+  key_word VARCHAR(15) NOT NULL,
+  PRIMARY KEY (book_id), 
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS library (
+library_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+library_name VARCHAR(45) NOT NULL,
+library_postal_code CHAR(5) NOT NULL
+library_phone INT UNSIGNED NOT NULL,
+library_email VARCHAR(45) NOT NULL,
+date_of_construction DATE NOT NULL,
+number_of_books INT UNSIGNED NOT NULL,
+PRIMARY KEY (library_id),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS student (
+  student_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  student_name VARCHAR(15) NOT NULL,
+  student_surname VARCHAR(15) NOT NULL,
+  student_age INT UNSIGNED NOT NULL,
+  student_email VARCHAR(45) NOT NULL,
+  student_class INT UNSIGNED NOT NULL,
+  student_sex ENUM('M','F','NB') NOT NULL,
+  PRIMARY KEY (student_id),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS proffessor (
+  proffessor_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  proffessor_name  VARCHAR(15) NOT NULL,
+  proffessor_surname VARCHAR(15) NOT NULL,
+  proffessor_age INT UNSIGNED NOT NULL,
+  proffessor_email VARCHAR(45) NOT NULL,
+  proffessor_class INT UNSIGNED NOT NULL,
+  proffessor_sex ENUM('M','F','NB') NOT NULL,
+  PRIMARY KEY (proffessor_id),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS school_operator (
+  school_operator_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  school_operator_name VARCHAR(15) NOT NULL,
+  school_operator_surname VARCHAR(15) NOT NULL,
+  school_operator_age INT UNSIGNED NOT NULL,
+  school_operator_postal_code CHAR(5) NOT NULL,
+  school_operator_phone INT UNSIGNED NOT NULL,
+  school_operator_email VARCHAR(45) NOT NULL,
+  school_operator_sex ENUM('M','F','NB') NOT NULL,
+  PRIMARY KEY (school_operator_id),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS general_operator(
+  general_operator_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  general_operator_name VARCHAR(15) NOT NULL,
+  general_operator_surname VARCHAR(15) NOT NULL,
+  general_operator_age INT UNSIGNED NOT NULL,
+  general_operator_phone INT UNSIGNED NOT NULL,
+  general_operator_postal_code INT UNSIGNED NOT NULL,
+  general_operator_email VARCHAR(45) NOT NULL,
+  general_operator_sex ENUM('M','F','NB') NOT NULL,
+  PRIMARY KEY(general_operator_id),
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
