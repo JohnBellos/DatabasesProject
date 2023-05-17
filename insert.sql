d0e29a1fcdbcc557e787358e03a9385757bb4f55
ntua-el19952 <el19952@mail.ntua.gr>
2023-05-17 19:08:07 +0300

    fix again again

--- insert.sql
+++ insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO school (school_name, postal_code, city, phone, email, principal_name, operator_name)
 VALUES ('Example School', '12345', 'New York', '1234567890', 'example@email.com', 'John Smith', 'Jane Doe'),
-       ('Example School2', '12342', 'New York2', '12345678902', 'example@email.com2', 'John Smith2', 'Jane Doe2');
-INSERT INTO student (student_id, student_name, student_surname, student_age, student_email, student_class, student_sex)
+       ('Example School2', '12342', 'New York2', '1234567892', 'example@email.com2', 'John Smith2', 'Jane Doe2');
+INSERT INTO student (student_id, student_name, student_surname, student_email, student_age, student_sex, student_class)
 VALUES ('1','Shea','Lampert','slampert0@webeden.co.uk','12','Male','12'),
        ('2','Currie','Deacon','cdeacon1@woothemes.com','11','Male','8'),
        ('3','Carmella','Gamell','cgamell2@vinaora.com','10','Female','8'),
