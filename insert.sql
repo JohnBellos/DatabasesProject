09f3e9ebe30fc36aca2f018d087c06d41e0533a2
billygds <vasilisgoudis1@gmail.com>
2023-05-17 18:59:17 +0300

    test2

--- insert.sql
+++ insert.sql
@@ -1,3 +1,2 @@
 INSERT INTO school (school_name, postal_code, city, phone, email, principal_name, operator_name)
 VALUES ('Example School', '12345', 'New York', '1234567890', 'example@email.com', 'John Smith', 'Jane Doe');
-       ('Example School2', '123452', 'New York2', '12345678902', 'example@email.com2', 'John Smith2', 'Jane Doe2');
