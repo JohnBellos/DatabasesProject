40f7a524064ddde4708565f688ae23c5b7de3fa7
billygds <vasilisgoudis1@gmail.com>
2023-05-17 18:56:00 +0300

    test

--- insert.sql
+++ insert.sql
@@ -1,11 +1,3 @@
 INSERT INTO school (school_name, postal_code, city, phone, email, principal_name, operator_name)
 VALUES ('Example School', '12345', 'New York', '1234567890', 'example@email.com', 'John Smith', 'Jane Doe');
        ('Example School2', '123452', 'New York2', '12345678902', 'example@email.com2', 'John Smith2', 'Jane Doe2');
-
-
-INSERT INTO student (student_id, student_name, student_surname, student_age, student_email, student_class, student_sex)
-VALUES ('1','Shea','Lampert','slampert0@webeden.co.uk','12','Male','12')
-       ('2','Currie','Deacon','cdeacon1@woothemes.com','11','Male','8')
-       ('3','Carmella','Gamell','cgamell2@vinaora.com','10','Female','8')
-       ('4','Briny,Mallett','bmallett3@amazon.de','7','Female','12')
-       ('5','Willi','Robardey','wrobardey4@scribd.com','11','Female','8')
