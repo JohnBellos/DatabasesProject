1954373914a284d75fca590256244b04b4aff0dd
ntua-el19952 <el19952@mail.ntua.gr>
2023-05-17 19:11:08 +0300

    fix again^3

--- insert.sql
+++ insert.sql
@@ -2,8 +2,8 @@ INSERT INTO school (school_name, postal_code, city, phone, email, principal_name
 VALUES ('Example School', '12345', 'New York', '1234567890', 'example@email.com', 'John Smith', 'Jane Doe'),
        ('Example School2', '12342', 'New York2', '1234567892', 'example@email.com2', 'John Smith2', 'Jane Doe2');
 INSERT INTO student (student_id, student_name, student_surname, student_email, student_age, student_sex, student_class)
-VALUES ('1','Shea','Lampert','slampert0@webeden.co.uk','12','Male','12'),
-       ('2','Currie','Deacon','cdeacon1@woothemes.com','11','Male','8'),
-       ('3','Carmella','Gamell','cgamell2@vinaora.com','10','Female','8'),
-       ('4','Briny,Mallett','bmallett3@amazon.de','7','Female','12'),
-       ('5','Willi','Robardey','wrobardey4@scribd.com','11','Female','8');
+VALUES ('1','Shea','Lampert','slampert0@webeden.co.uk','12','M','12'),
+       ('2','Currie','Deacon','cdeacon1@woothemes.com','11','M','8'),
+       ('3','Carmella','Gamell','cgamell2@vinaora.com','10','F','8'),
+       ('4','Briny','Mallett','bmallett3@amazon.de','7','F','12'),
+       ('5','Willi','Robardey','wrobardey4@scribd.com','11','F','8');
