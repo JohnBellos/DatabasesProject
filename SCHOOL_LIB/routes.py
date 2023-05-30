6c6aecf1b9a1c628b09549eaa7babaefbf2ecd79
ntua-el19952 <el19952@mail.ntua.gr>
2023-05-31 02:39:59 +0300

    everyone gets their own dashboard/edit_info

--- SCHOOL_LIB/routes.py
+++ SCHOOL_LIB/routes.py
@@ -108,59 +108,60 @@ def login():
     return render_template("login.html")
 
 
-@app.route('/dashboard', methods=['POST'])
+@app.route('/dashboard', methods=['POST', 'GET'])
 def dashboard():
-    username = request.cookies.get('id')
-    username = request.form['username']
-    password = request.form['password']
-    user = authentication(username, password)
-    print("User(or not) identified")
-    print(user)
-    if user == []:
-        print("I am nobody")
-        return render_template("login.html")
-    else:
-        # user = [60, 'valeveque9', 'RpHeZAR', 'Valentine', 'Aleveque', 'valeveque9@arstechnica.com', '15', 'F', '9', 'professor', 2]
-        
-        if len(user) == 10:
-            print("I am an admin")
-            # it is the admin
-            webpage = render_template("dashboardAdmin.html", user = user)
-            resp = make_response(webpage)
-            resp.set_cookie('id', 'admin')
-            return resp
-        if user[11] == 'professor':
-            print("I am a Professor")
-            query = '''SELECT is_operator
-                    FROM library_user
-                    WHERE user_name = '{}' AND user_surname = '{}';'''.format(user[3], user[4])
-           
-            cur = db.connection.cursor()
-            cur.execute(query)
-            rv = cur.fetchall()
+    if request.method == 'POST':
+        username = request.cookies.get('id')
+        username = request.form['username']
+        password = request.form['password']
+        user = authentication(username, password)
+        print("User(or not) identified")
+        print(user)
+        if user == []:
+            print("I am nobody")
+            return render_template("login.html")
+        else:
+            # user = [60, 'valeveque9', 'RpHeZAR', 'Valentine', 'Aleveque', 'valeveque9@arstechnica.com', '15', 'F', '9', 'professor', 2]
             
-            print(rv)
-            isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
-            if isOperator == 1:
-                print("I am a Professor and an Operator")
-                webpage = render_template("dashboardOp.html", user = user)
+            if len(user) == 10:
+                print("I am an admin")
+                # it is the admin
+                webpage = render_template("dashboardAdmin.html", user = user)
                 resp = make_response(webpage)
-                resp.set_cookie('id', str(user[0]))
+                resp.set_cookie('id', 'admin')
                 return resp
-            else:
-                webpage = render_template("dashboardProf.html", user = user)
-                resp = make_response(webpage)
-                resp.set_cookie('id', str(user[0]))
-                return resp
+            if user[11] == 'professor':
+                print("I am a Professor")
+                query = '''SELECT is_operator
+                        FROM library_user
+                        WHERE user_name = '{}' AND user_surname = '{}';'''.format(user[3], user[4])
+            
+                cur = db.connection.cursor()
+                cur.execute(query)
+                rv = cur.fetchall()
+                
+                print(rv)
+                isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
+                if isOperator == 1:
+                    print("I am a Professor and an Operator")
+                    webpage = render_template("dashboardOp.html", user = user)
+                    resp = make_response(webpage)
+                    resp.set_cookie('id', str(user[0]))
+                    return resp
+                else:
+                    webpage = render_template("dashboardProf.html", user = user)
+                    resp = make_response(webpage)
+                    resp.set_cookie('id', str(user[0]))
+                    return resp
 
-        # user = [1, 'jlefleming0', 'aovGiL', 'Jonah', 'Le Fleming', 'jlefleming0@usnews.com', '7', 'M', '12', 'student', 2]
-        if user[11] == 'student':
-            print("I am a Student")
-            webpage = render_template("dashboardStd.html", user = user)
-            resp = make_response(webpage)
-            resp.set_cookie('id', str(user[0]))
-            return resp
-    return "Invalid User Type"
+            # user = [1, 'jlefleming0', 'aovGiL', 'Jonah', 'Le Fleming', 'jlefleming0@usnews.com', '7', 'M', '12', 'student', 2]
+            if user[11] == 'student':
+                print("I am a Student")
+                webpage = render_template("dashboardStd.html", user = user)
+                resp = make_response(webpage)
+                resp.set_cookie('id', str(user[0]))
+                return resp
+        return "Invalid User Type"
 
 def authentication(username, password):
     
@@ -195,8 +196,12 @@ def edit_info():
     cur.execute(query)
     rv = cur.fetchall()
     user = [item for sublist in rv for item in sublist]
+    print(user)
+    if user[11] == 'student':
+        return render_template('edit_info_std.html', user = user)
+    else:
+        return render_template('edit_info.html', user = user)
     
-    return render_template('edit_info.html', user = user)
 
 @app.route('/save_info', methods = ['POST'])
 def save_info():
@@ -206,12 +211,13 @@ def save_info():
     name = request.form['name']
     surname = request.form['surname']
     email = request.form['email']
+    sex = request.form['sex']
+    age = request.form['age']
     uclass = request.form['class']
-    name = request.form['name']
 
     # update database
     # show /dashboard
-    query = "UPDATE library_user SET username = '{}', user_password = '{}', user_email = '{}', user_class = '{}' WHERE user_id = {};".format(username, password, email, uclass, id)
+    query = "UPDATE library_user SET username = '{}', user_password = '{}', user_email = '{}', user_class = '{}', user_name = '{}', user_surname = '{}', user_age = {}, user_sex = '{}' WHERE user_id = {};".format(username, password, email, uclass, name, surname, age, sex, id)
     print(query)
     cur = db.connection.cursor()
     cur.execute(query)
@@ -223,7 +229,34 @@ def save_info():
     cur.execute(query)
     rv = cur.fetchall()
     user = [item for sublist in rv for item in sublist]
-    return render_template('dashboard.html', user = user)
+    print(user)
+    if user[11] == 'student':
+        return render_template('dashboardStd.html', user = user)
+    else:
+        if user[11] == 'professor':
+            print("I am a Professor")
+            query = '''SELECT is_operator
+                    FROM library_user
+                    WHERE user_name = '{}' AND user_surname = '{}';'''.format(user[3], user[4])
+           
+            cur = db.connection.cursor()
+            cur.execute(query)
+            rv = cur.fetchall()
+            
+            print(rv)
+            isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
+            if isOperator == 1:
+                print("I am a Professor and an Operator")
+                webpage = render_template("dashboardOp.html", user = user)
+                resp = make_response(webpage)
+                resp.set_cookie('id', str(user[0]))
+                return resp
+            else:
+                webpage = render_template("dashboardProf.html", user = user)
+                resp = make_response(webpage)
+                resp.set_cookie('id', str(user[0]))
+                return resp
+
 
 @app.route('/register')
 def register():
