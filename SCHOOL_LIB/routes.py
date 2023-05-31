3a4e3a8f63a99bbb7129287318caaaebd4c10115
John Bellos <johnbellos77@gmail.com>
2023-05-31 03:44:52 +0300

    dashboard cleaning, reorganization

--- SCHOOL_LIB/routes.py
+++ SCHOOL_LIB/routes.py
@@ -87,60 +87,116 @@ def bookView(book_id):
 def login():
     return render_template("login.html")
 
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    username = request.form['username']
+    password = request.form['password']
+    user = authentication(username, password)
+    if user == []:
+        print("I am nobody")
+        return render_template("login.html")
+    
+
 @app.route('/dashboard', methods=['POST', 'GET'])
 def dashboard():
-    if request.method == 'POST':
-        username = request.cookies.get('id')
+    print('start debug')
+    if request.method == 'GET':
+        print('get')
+        uid = request.cookies.get('id')
+        if uid == 'admin':
+            query = "SELECT * FROM administrator WHERE administrator_id = 1;"
+        else:
+            query = "SELECT * FROM library_user WHERE user_id = {};".format(uid)
+        print(query)
+        cur = db.connection.cursor()
+        cur.execute(query)
+        user = cur.fetchall()
+        print(user)
+        user = list(user[0])
+        if user == []: # authentication failed
+            print("I am nobody")
+            return render_template("login.html") 
+        print('check1')
+        return show_dashboard(user)
+    
+    elif request.method == 'POST':
+        print('post')
         username = request.form['username']
         password = request.form['password']
         user = authentication(username, password)
-        print("User(or not) identified")
-        print(user)
-        if user == []:
+        if user == []: # authentication failed
             print("I am nobody")
             return render_template("login.html")
-        else:
-            # user = [60, 'valeveque9', 'RpHeZAR', 'Valentine', 'Aleveque', 'valeveque9@arstechnica.com', '15', 'F', '9', 'professor', 2]
-            
-            if len(user) == 10:
-                print("I am an admin")
-                # it is the admin
-                webpage = render_template("dashboardAdmin.html", user = user)
-                resp = make_response(webpage)
-                resp.set_cookie('id', 'admin')
-                return resp
-            if user[11] == 'professor':
-                print("I am a Professor")
-                query = '''SELECT is_operator
-                        FROM library_user
-                        WHERE user_name = '{}' AND user_surname = '{}';'''.format(user[3], user[4])
-            
-                cur = db.connection.cursor()
-                cur.execute(query)
-                rv = cur.fetchall()
-                
-                print(rv)
-                isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
-                if isOperator == 1:
-                    print("I am a Professor and an Operator")
-                    webpage = render_template("dashboardOp.html", user = user)
-                    resp = make_response(webpage)
-                    resp.set_cookie('id', str(user[0]))
-                    return resp
-                else:
-                    webpage = render_template("dashboardProf.html", user = user)
-                    resp = make_response(webpage)
-                    resp.set_cookie('id', str(user[0]))
-                    return resp
+        else: # authentication successful
+            return show_dashboard_with_cookie_creation(user)
 
-            # user = [1, 'jlefleming0', 'aovGiL', 'Jonah', 'Le Fleming', 'jlefleming0@usnews.com', '7', 'M', '12', 'student', 2]
-            if user[11] == 'student':
-                print("I am a Student")
-                webpage = render_template("dashboardStd.html", user = user)
-                resp = make_response(webpage)
-                resp.set_cookie('id', str(user[0]))
-                return resp
-        return "Invalid User Type"
+def show_dashboard(user):
+    print('check boobob')
+    print(user)
+    if len(user) == 10:
+        # it is the admin
+        print('check21')
+        return render_template("dashboardAdmin.html", user = user)
+    elif user[11] == 'professor':
+        query = 'SELECT is_operator FROM library_user WHERE user_id = {}'.format(user[0])
+        cur = db.connection.cursor()
+        cur.execute(query)
+        rv = cur.fetchall()
+        isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
+        if isOperator == 1:
+            # Operator
+            return render_template("dashboardOp.html", user = user)
+        else:
+            # Teacher but not operator
+            return render_template("dashboardProf.html", user = user)
+    if user[11] == 'student':
+        print("I am a Student")
+        return render_template("dashboardStd.html", user = user)
+
+
+def show_dashboard_with_cookie_creation(user):
+    print("User(or not) identified")
+    print(user)
+    
+    # user = [60, 'valeveque9', 'RpHeZAR', 'Valentine', 'Aleveque', 'valeveque9@arstechnica.com', '15', 'F', '9', 'professor', 2]
+    if len(user) == 10:
+        print("I am an admin")
+        # it is the admin
+        webpage = render_template("dashboardAdmin.html", user = user)
+        resp = make_response(webpage)
+        resp.set_cookie('id', 'admin')
+        return resp
+    if user[11] == 'professor':
+        print("I am a Professor")
+        query = '''SELECT is_operator
+                FROM library_user
+                WHERE user_name = '{}' AND user_surname = '{}';'''.format(user[3], user[4])
+    
+        cur = db.connection.cursor()
+        cur.execute(query)
+        rv = cur.fetchall()
+        
+        print(rv)
+        isOperator = rv[0][0]  # 1 if Operator - 0 if not Operator
+        if isOperator == 1:
+            print("I am a Professor and an Operator")
+            webpage = render_template("dashboardOp.html", user = user)
+            resp = make_response(webpage)
+            resp.set_cookie('id', str(user[0]))
+            return resp
+        else:
+            webpage = render_template("dashboardProf.html", user = user)
+            resp = make_response(webpage)
+            resp.set_cookie('id', str(user[0]))
+            return resp
+    # user = [1, 'jlefleming0', 'aovGiL', 'Jonah', 'Le Fleming', 'jlefleming0@usnews.com', '7', 'M', '12', 'student', 2]
+    if user[11] == 'student':
+        print("I am a Student")
+        webpage = render_template("dashboardStd.html", user = user)
+        resp = make_response(webpage)
+        resp.set_cookie('id', str(user[0]))
+        return resp
+    return "Invalid User Type"
 
 
 def authentication(username, password):
@@ -164,5 +220,4 @@ def authentication(username, password):
         
         user = [item for sublist in rv for item in sublist]
        
-    
     return user
